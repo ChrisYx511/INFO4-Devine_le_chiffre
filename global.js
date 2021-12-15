8fdbc623969c37562a70f4df5007f64d71c8c21c
Chris Yang <58494380+ChrisYx511@users.noreply.github.com>
2021-12-15 01:33:21 -0500

    Finish Project

--- global.js
+++ global.js
@@ -1,28 +1,41 @@
-var tutorialPageNumber = 1
-var selectedDifficulty = ""
-var overallGameProgress = 0
+let tutorialPageNumber = 1
+let selectedDifficulty = ""
+let overallGameProgress = 0
 let healthpoints = 84
+let correctRandomNumber = 0
+let randomNumberRange = []
+let ennemyHealth = 3
 
 const menuClickSound = new Audio("./assets/sounds/sfx/click.wav")
 const lastSupriseMusic = new Audio("./assets/sounds/music/lastsuprise.mp3")
 const battleStartSound = new Audio("./assets/sounds/sfx/battlestart.wav")
 const personaVoiceSound = new Audio("./assets/sounds/sfx/persona.wav")
 const menuCancelSound = new Audio("./assets/sounds/sfx/menucancel.wav")
-
+const damageTakenSound = new Audio("./assets/sounds/sfx/damagetaken.wav")
+const hitSound = new Audio("./assets/sounds/sfx/hit.wav")
+const criticalSound = new Audio("./assets/sounds/sfx/critical.mp3")
+const menuSelectSound = new Audio("./assets/sounds/sfx/menuselect.wav")
+const victoryMusic = new Audio("./assets/sounds/music/victory.flac")
+const velvetRoomMusic = new Audio("./assets/sounds/music/velvetroom.flac")
 const allGameContainers = document.querySelector(".gameContainer")
+
 const gameContainerPreSkillSelect = document.querySelector("#gameContainerPreSkillSelect")
 const gameContainerLoadingScreen = document.querySelector("#GameContainerLoadingScreen")
 const gameContainerSkillSelect = document.querySelector("#gameContainerSkillSelect")
+const gameContainerNumberGuess = document.querySelector("#gameContainerGuessNumber")
+const gameContainerVictoryScreen = document.querySelector("#gameContainerVictoryScreen")
+const gameContainerGameOverScreen = document.querySelector("#gameContainerGameOverScreen")
 
 document.addEventListener("keyup", (event) => {
     if (event.key === "Enter") {
         processEnterKey()
-        console.log("Enter detected")
+        //console.log("Enter detected")
     } else if (event.key === "Escape") {
         processEscKey()
-        console.log("Escape Detected")
+        //console.log("Escape Detected")
     }
 })
+
 
 function processEnterKey() {
     switch (overallGameProgress) {
@@ -35,6 +48,9 @@ function processEnterKey() {
         case 2 :
             openNumberGuess()
             break;
+        case 5 :
+            location.reload()
+            break;
     }
 }
 
@@ -45,6 +61,14 @@ function processEscKey() {
             gameContainerPreSkillSelect.style.display = "inherit"
             menuCancelSound.play()
             overallGameProgress = 1
+            break;
+        case 3 :
+            gameContainerNumberGuess.style.display = "none"
+            gameContainerSkillSelect.style.display = "inherit"
+            menuCancelSound.play()
+            randomNumberRange = []
+            overallGameProgress = 2
+            break;
     }
 }
 
@@ -78,24 +102,24 @@ function battleStart(difficulty) {
     console.log("This shit works " + difficulty)
     switch(difficulty) {
         case "Facile" :
-            selectedDifficulty = 0
+            selectedDifficulty = 2
             break;
         case "Moyen" :
             selectedDifficulty = 1
             break;
         case "Difficile" :
-            selectedDifficulty = 2
+            selectedDifficulty = 0
             break;
     }
     console.log("Selected Difficulty: " + selectedDifficulty)
     switch (selectedDifficulty) {
-        case 0 :
+        case 2 :
             healthpoints = 84
             break;
         case 1 :
             healthpoints = 64
             break;
-        case 2 :
+        case 0 :
             healthpoints = 44
             break;
     }
@@ -104,7 +128,7 @@ function battleStart(difficulty) {
 
     menuClickSound.play()
     battleStartSound.play()
-    lastSupriseMusic.volume = 0.5
+    lastSupriseMusic.volume = 0.75
     lastSupriseMusic.play()
     lastSupriseMusic.loop = true
 
@@ -118,6 +142,7 @@ function battleStart(difficulty) {
 function skillSelect() {
     const healthpointsDiv = document.querySelector("#hpCountSkillSelect")
     healthpointsDiv.innerHTML = String(healthpoints)
+    menuSelectSound.play()
     personaVoiceSound.play()
     gameContainerPreSkillSelect.style.display = "none"
     gameContainerSkillSelect.style.display = "inherit"
@@ -125,15 +150,145 @@ function skillSelect() {
 }
 
 
+function openNumberGuess() {
+    gameContainerSkillSelect.style.display = "none"
+    gameContainerNumberGuess.style.display = "inherit"
+    overallGameProgress = 3
+    menuSelectSound.play()
+    correctRandomNumber = getRandomInt(1, 100)
+    randomNumberRange.push(correctRandomNumber-(5*(selectedDifficulty+1)))
+    if (randomNumberRange[0] <= 0) {
+        randomNumberRange[0] = 1
+    }
+    randomNumberRange.push(correctRandomNumber+(5*(selectedDifficulty+1))) 
+    if (randomNumberRange[1] >= 100) {
+        randomNumberRange[1] = 100
+    } 
+
+    console.log("Correct Answer: " + correctRandomNumber)
+    console.log("Random Number Range: " + randomNumberRange)
+}
+
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
-  }  
+}  
 
-
-
-const correctNumber = getRandomInt(1, 100)
+let attacktype = 0
 function validate() { 
-    if (document.getElementById("answer").value == correctNumber) {
-        alert("ggmjiomj")
+    let answer = document.querySelector("#guessedNumberInput").value
+    menuSelectSound.play()
+
+    if (answer == correctRandomNumber) {
+        attacktype = 2
+        console.log("Critical Hit!, Number guessed exactly.")
+    } else if (answer >= randomNumberRange[0] && answer <= randomNumberRange[1]) {
+        attacktype = 1
+        console.log("Hit!, Number guessed within range")
+    } else {
+        attacktype = 0
+        console.log("Block!")
+        healthpoints = healthpoints-10
+        if (healthpoints < 0) {
+            healthpoints = 0
+        }
     }
+    returnToBattle()
+}
+
+function returnToBattle() {
+    overallGameProgress = 4
+    gameContainerNumberGuess.style.display = "none"
+    gameContainerPreSkillSelect.style.display = "inherit"
+    const healthpointsDiv1 = document.querySelector("#hpCountPreSkillSelect")
+    const healthpointsDiv2 = document.querySelector("#hpCountSkillSelect")
+    randomNumberRange = []
+    switch (attacktype) {
+        case 0 :
+            damageTakenSound.play()
+            healthpointsDiv1.innerHTML = String(healthpoints)
+            healthpointsDiv2.innerHTML = String(healthpoints)
+            healthpointsDiv1.style.animationName = "shake"
+            healthpointsDiv1.style.animationDuration = "1s"
+            healthpointsDiv1.style.animationIterationCount = "1"
+            setTimeout(function() {
+                healthpointsDiv1.style.removeProperty("animation-name")
+                healthpointsDiv1.style.removeProperty("animation-duration")
+                healthpointsDiv1.style.removeProperty("animation-iteration-count")
+            }, 1000);
+            document.querySelector(".hitMarkBlock img").style.display = "inherit"
+            setTimeout(function() {
+                document.querySelector(".hitMarkBlock img").style.display = "none"
+            }, 1500);
+            if (healthpoints == 0) {
+                overallGameProgress = 5
+                gameOverSequence()
+            } else {
+                overallGameProgress = 1
+            }
+            break;
+        case 1 : 
+            hitSound.play()
+            ennemyHealth = ennemyHealth - 1
+            switch (ennemyHealth) {
+                case 2 :
+                    gameContainerPreSkillSelect.style.backgroundImage = "url(./assets/battle-screen-2-3-health.png)"
+                    break;
+                case 1 :
+                    gameContainerPreSkillSelect.style.backgroundImage = "url(./assets/battle-screen-1-3-health.png)"
+                    break;
+                case 0 :
+                    gameContainerPreSkillSelect.style.backgroundImage = "url(./assets/battle-screen-no-health.png)"
+                    break;
+            }
+            if (ennemyHealth == 0) {
+                overallGameProgress = 5
+                victorySequence()
+            } else {
+                overallGameProgress = 1
+            }
+            break;
+        case 2 :
+            criticalSound.play()
+            document.querySelector(".hitMarkCriticalCutIn img").style.display = "inherit";
+            setTimeout(function() {
+                document.querySelector(".hitMarkCriticalCutIn img").style.display = "none"
+                hitSound.play()
+                document.querySelector(".hitMarkCritical img").style.display = "inherit";
+                setTimeout(function() {
+                    document.querySelector(".hitMarkCritical img").style.display = "none";
+                }, 1500)
+                gameContainerPreSkillSelect.style.backgroundImage = "url(./assets/battle-screen-no-health.png)"
+                ennemyHealth = 0
+                overallGameProgress = 5
+                
+                victorySequence()
+            }, 1200);
+            
+            
+    }
+}
+
+function victorySequence() {
+    lastSupriseMusic.pause()
+    lastSupriseMusic.currentTime = 0
+    setTimeout(function(){
+        gameContainerPreSkillSelect.style.display = "none"
+        gameContainerVictoryScreen.style.display = "inherit"
+        gameContainerVictoryScreen.style.backgroundImage = "url(./assets/victoryscreen.gif)"
+        victoryMusic.loop = true
+        victoryMusic.volume = 0.75
+        victoryMusic.play()
+    }, 2000)
+}
+
+function gameOverSequence() {
+    lastSupriseMusic.pause()
+    lastSupriseMusic.currentTime = 0
+    setTimeout(function(){
+        gameContainerPreSkillSelect.style.display = "none"
+        gameContainerGameOverScreen.style.display = "inherit"
+        velvetRoomMusic.loop = true
+        velvetRoomMusic.volume = 0.75
+        velvetRoomMusic.play()
+    }, 2000)
 }
--- todo.md
+++ todo.md
@@ -1,6 +1,7 @@
 ## To Do
-- Finish Skill Select
+- ~~Finish Skill Select~~
 - Implement Number Guessing prompt
+- Implement ennnemy health logic
 - Victory screen, restart, etc.
 - Health Bar
 - Style Buttons and other inputs
